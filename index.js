f980d43422ed3734b10b705e2829a19216c09d5c
iamsurge <112551585+iamsurge@users.noreply.github.com>
2022-10-10 12:32:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://joelee.works');
   await page.screenshot({path: 'example.png'});
-
   await browser.close();
 })();
